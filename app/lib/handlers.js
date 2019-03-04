d2d81526dc5ee8dd6dcb885cf9e9bfda53641023
Jeezman <tobiadeyemi91@gmail.com>
2019-03-04 13:14:52 +0100

    Implement get user

--- app/lib/handlers.js
+++ app/lib/handlers.js
@@ -96,7 +96,26 @@ handlers._users.post = function(data, callback) {
   }
 };
 
-handlers._users.get = function(data, callback) {};
+handlers._users.get = function(data, callback) {
+  var phone =
+    typeof data.queryStringObject.phone == 'string' &&
+    data.queryStringObject.phone.trim().length == 11
+      ? data.queryStringObject.phone.trim()
+      : false;
+
+  if (phone) {
+    _data.read('users', phone, function(err, data) {
+      if (!err && data) {
+        delete data.hashedPassword;
+        callback(200, data);
+      } else {
+        callback(404);
+      }
+    });
+  } else {
+    callback(400, { Error: 'Missing required field' });
+  }
+};
 
 handlers._users.put = function(data, callback) {};
 
