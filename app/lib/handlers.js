bffac3de9aff3db23b3c06dae5e8282b268fa680
Jeezman <tobiadeyemi91@gmail.com>
2019-03-06 13:08:56 +0100

    Implement update

--- app/lib/handlers.js
+++ app/lib/handlers.js
@@ -117,7 +117,59 @@ handlers._users.get = function(data, callback) {
   }
 };
 
-handlers._users.put = function(data, callback) {};
+handlers._users.put = function(data, callback) {
+  var phone =
+    typeof data.payload.phone == 'string' &&
+    data.payload.phone.trim().length == 11
+      ? data.payload.phone.trim()
+      : false;
+
+  var firstName =
+    typeof data.payload.firstName == 'string' &&
+    data.payload.firstName.trim().length > 0
+      ? data.payload.firstName.trim()
+      : false;
+
+  var lastName =
+    typeof data.payload.lastName == 'string' &&
+    data.payload.lastName.trim().length > 0
+      ? data.payload.lastName.trim()
+      : false;
+
+  var password =
+    typeof data.payload.password == 'string' &&
+    data.payload.password.trim().length > 0
+      ? data.payload.password.trim()
+      : false;
+
+  if (phone) {
+    if (firstName || lastName || password) {
+      _data.read('users', phone, function(err, userData) {
+        console.log('user data is ', userData);
+        if (!err && userData) {
+          if (firstName) userData.firstName = firstName;
+          if (lastName) userData.lastName = lastName;
+          if (password) userData.hashedPassword = helpers.hash(password);
+
+          _data.update('users', phone, userData, function(err) {
+            if (!err) {
+              callback(200);
+            } else {
+              console.log(err);
+              callback(500, { Error: 'Could not update the user' });
+            }
+          });
+        } else {
+          callback(400, { Error: 'The specified user does not exist' });
+        }
+      });
+    } else {
+      callback(400, { Error: 'Missing required fields to update' });
+    }
+  } else {
+    callback(400, { Error: 'Missing required field' });
+  }
+};
 
 handlers._users.delete = function(data, callback) {};
 
